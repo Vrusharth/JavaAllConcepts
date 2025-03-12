ffab14ea954d28f7dc0b4deb3a4778218109e216
Vrusharth Nirmal <vrusharth2003@gmail.com>
2025-03-12 14:00:56 +0530

    Access modifiers

---
+++ src/com/company/AccessModifiers.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+class learn{
+    private int nums;
+
+    public int getNums() {
+        return nums;
+    }
+
+    public void setNums(int nums) {
+        this.nums = nums;
+    }
+
+    public learn(int nums) {
+        System.out.println(nums);
+    }
+}
+public class AccessModifiers {
+    public static void main(String[] args) {
+        int num;
+        learn uk=new learn(12);
+
+
+
+    }
+}
